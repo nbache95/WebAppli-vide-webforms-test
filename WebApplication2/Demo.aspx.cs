edcd7573e402418d9e2a6a2de029e3fd915ece8d
Nour Bache <nour.bache@etu.cyu.fr>
2021-02-11 16:16:39 +0100

    ajout de sesson Ajout de la connexion a la bdd Ajout d'une requête SQL pour accéder aux éléments de la table demotb

--- WebApplication2/Demo.aspx.cs
+++ WebApplication2/Demo.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,21 @@ namespace WebApplication2
 {
     public partial class Demo : System.Web.UI.Page
     {
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            string connectionString ;
+            SqlConnection cnn;
 
+            connectionString = @"Server=CY-RT85O0N2T5PK\MSSQlSERVER01;Trusted_Connection=True;Database=mydbtuto ;User ID=demo_user;Password=Password123";
+
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+
+            Response.Write("Connection Made");
+            cnn.Close();
         }
 
         protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
@@ -21,6 +34,32 @@ namespace WebApplication2
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
+            cnn.Open();
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = " ";
+            sql = "Select TutorialID,TutorialName from demotb";
+
+            command = new SqlCommand(sql, cnn);
+
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + "-" + dataReader.GetValue(1) + "</br>";
+            }
+
+            Response.Write(Output);
+            dataReader.Close();
+            command.Dispose();
+
+            cnn.Close();
+
+
+            //Session["Name"] = txtName.Text;
+
+            Response.Write(Session["Name"]);
+            
             Response.Write(txtName.Text + "</br>");
             Response.Write(lstlocation.SelectedItem.Text + "</br>");
 
